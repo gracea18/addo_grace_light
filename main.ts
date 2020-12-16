f1f4d549b373c299f1dcee02d0aec52d3dff0f20
gracea18 <72771341+gracea18@users.noreply.github.com>
2020-12-15 23:12:37 -0500

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("Light Level" + input.lightLevel())
-    if (input.lightLevel() > 20) {
+    if (input.lightLevel() > 15) {
         light.setAll(light.rgb(0, 0, 0))
     } else {
         light.setAll(light.rgb(255, 255, 255))
